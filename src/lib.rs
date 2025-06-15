5d968847c2f012e9b65d7266cb4c695d1ff03d54
Alex Colby <alexanderdcolby@gmail.com>
2025-06-15 17:33:13 +0100

    docs: adds badges for Github link and package version

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-//! # Constraint Satisfaction Solving
+//! [![Github homepage](https://img.shields.io/badge/github-Just--Helpful%2Ffarc3-brightgreend?style=for-the-badge&logo=github)](https://github.com/Just-Helpful/farc3)
+//! ![Package version](https://img.shields.io/badge/dynamic/toml?url=https%3A%2F%2Fraw.githubusercontent.com%2FJust-Helpful%2Ffarc3-csp%2Frefs%2Fheads%2Fmain%2FCargo.toml&query=%24.package.version&prefix=v&style=for-the-badge&logo=rust&label=crates.io&color=%23FF642D)
 //!
-//! This crate is focused on a semi-generic approach to solving Constraint Satisfaction Problems,\
+//! A semi-generic approach to solving Constraint Satisfaction Problems,\
 //! with the possibility to optimise based on the specific implementation of Constraints.
 //!
 //! The primary exports of this crate are:

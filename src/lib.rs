5c3ea4cc1f9467d9c40057324c4c80dda290e296
Just-Helpful <47320175+Just-Helpful@users.noreply.github.com>
2025-06-22 21:23:26 +0000

    🔄 Update badge

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@
 //! )
 //! [![Package version](https://raw.githubusercontent.com/Just-Helpful/Farc3/refs/heads/main/.github/badges/coverage.svg)][coverage-url]
 //!
-//! [coverage-url]: my-url
+//! [coverage-url]: https://github.com/Just-Helpful/Farc3/actions/runs/15810924967/artifacts/3378831155
 //!
 //! A semi-generic approach to solving Constraint Satisfaction Problems,\
 //! with the possibility to optimise based on the specific implementation of Constraints.
